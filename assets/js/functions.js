73ecf6b75fcaba7a8ef342c434150b1a41b36bc1
Luis Acosta <37425100+luistaurik@users.noreply.github.com>
2023-06-11 16:31:42 -0500

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,180 @@
+function showMap() {
+    let initialDisplay, finalDisplay;
+    initialDisplay = document.querySelector("#map").style.display;
+    document.querySelector("#map").style.display = "block";
+    if (initialDisplay === "block" || initialDisplay === "flex") {
+    finalDisplay = "none";
+    } else {
+    finalDisplay = "block";
+    }
+    document.querySelector("#map").style.display = finalDisplay;
+}
+
+function showButtonIcon() {
+    let initialDisplay, finalDisplay;
+    initialDisplay = document.querySelector("#map-button").style.display;
+    finalDisplay = "block"
+    document.querySelector("#map-button").style.display = finalDisplay;
+}
+
+function showTutorial1() {
+    let initialDisplay, finalDisplay;
+    initialDisplay = document.querySelector("#tutorial-message").style.display;
+    finalDisplay = "block"
+    document.querySelector("#tutorial-message").style.display = finalDisplay;
+}
+
+function showMapButton() {
+    let initialDisplay, finalDisplay;
+    initialDisplay = document.querySelector("#map").style.display;
+    document.querySelector("#map").style.display = "block";
+    if (initialDisplay === "block" || initialDisplay === "flex") {
+    finalDisplay = "none";
+    } else {
+    finalDisplay = "block";
+    }
+    document.querySelector("#map").style.display = finalDisplay;
+}
+
+function countStickers1() {
+    let theAgile, notification;
+    notification = document.querySelector("#agile-number");
+    theAgile = document.querySelector("#the-agile").value;
+    if(theAgile > 10) {
+        notification.textContent = "You cannot have more than 10 members of this character"
+    }
+    else {
+        notification.textContent = "You have " + theAgile + " Members of this character"
+    };
+
+}   
+
+function countStickers2() {
+    let theCreative, notification;
+    notification = document.querySelector("#creative-number");
+    theCreative = document.querySelector("#the-creative").value;
+    if(theCreative > 10) {
+        notification.textContent = "You cannot have more than 10 members of this character"
+    }
+    else {
+        notification.textContent = "You have " + theCreative + " Members of this character"
+    };
+
+}  
+
+function countStickers3() {
+    let theStrong, notification;
+    notification = document.querySelector("#strong-number");
+    theCreative = document.querySelector("#the-strong").value;
+    if(theStrong > 10) {
+        notification.textContent = "You cannot have more than 10 members of this character"
+    }
+    else {
+        notification.textContent = "You have " + theCreative + " Members of this character"
+    };
+
+}  
+
+function countTotal() {
+    let agiles, creatives, strongs, calculateMessage, sumStickers;
+    agiles = document.querySelector("#the-agile").value;
+    creatives = document.querySelector("#the-creative").value;
+    strongs = document.querySelector("#the-strong").value;
+    calculateMessage = document.querySelector("#team-message");
+    sumStickers = Number(agiles) + Number(creatives) + Number(strongs);
+    if (sumStickers === 0 || agiles === "" || creatives === "" || strongs === "") {
+        calculateMessage.textContent = "You must have at least one member of each type in your team"
+    }
+    else if (sumStickers <= 10) {
+        calculateMessage.textContent = "You have " + sumStickers + " members in your team";
+    } 
+    else {
+        calculateMessage.textContent = "You cannot have more than 10 members in your team";
+    }
+}
+
+function countTotalCondition() {
+    let agiles, creatives, strongs, calculateMessage, sumStickers;
+    agiles = document.querySelector("#the-agile").value;
+    creatives = document.querySelector("#the-creative").value;
+    strongs = document.querySelector("#the-strong").value;
+    sumStickers = agiles + creatives + strongs;
+    calculateMessage = document.querySelector("#team-message");
+    if (agiles <= 10 && creatives <= 10 && strongs <= 10) {
+        countTotal();
+    }
+    else {
+        calculateMessage.textContent = "You cannot have more than 10 members in your team";
+    }
+}
+
+function changePin1() {
+    let padlockValue, padlockNumber,pinColor;
+    padlockValue = document.querySelector("#padlock-number1").value;
+    padlockNumber = parseFloat(padlockValue);
+    pinColor = document.querySelector("#pin-1");
+    if (padlockNumber == 9) {
+        pinColor.style.backgroundColor = "#21B35B";
+    } else {
+        pinColor.style.backgroundColor = "#263238";
+    }
+}
+
+function changePin2() {
+    let padlockValue, padlockNumber,pinColor;
+    padlockValue = document.querySelector("#padlock-number2").value;
+    padlockNumber = parseFloat(padlockValue);
+    pinColor = document.querySelector("#pin-2");
+    if (padlockNumber == 1) {
+        pinColor.style.backgroundColor = "#21B35B";
+    } else {
+        pinColor.style.backgroundColor = "#263238";
+    }
+}
+
+function changePin3() {
+    let padlockValue, padlockNumber,pinColor;
+    padlockValue = document.querySelector("#padlock-number3").value;
+    padlockNumber = parseFloat(padlockValue);
+    pinColor = document.querySelector("#pin-3");
+    if (padlockNumber == 1) {
+        pinColor.style.backgroundColor = "#21B35B";
+    } else {
+        pinColor.style.backgroundColor = "#263238";
+    }
+}
+
+function codeValidation() {
+    let padlockValue1, padlockValue2, padlockValue3, result, treasureImage, firstMessage;
+    let padlockNumber1, padlockNumber2, padlockNumber3;
+    result = document.querySelector("#game-result");
+    firstMessage = document.querySelector("treasure-title");
+    treasureImage = document.querySelector("#treasure-image");
+    padlockValue1 = document.querySelector("#padlock-number1").value;
+    padlockValue2 = document.querySelector("#padlock-number2").value;
+    padlockValue3 = document.querySelector("#padlock-number3").value;
+    padlockNumber1 = parseFloat(padlockValue1);
+    padlockNumber2 = parseFloat(padlockValue2);
+    padlockNumber3 = parseFloat(padlockValue3);
+    if (padlockNumber1 == 9 && padlockNumber2 == 1 && padlockNumber3 == 1) {
+        treasureImage.src = "/assets/imgs/open-treasure.png";
+        result.textContent = "You got the treasure 🥇🪙👑🫅";
+        firstMessage.display = "none";
+    }
+    else {
+        result.textContent = "You have not got the treasure yet 😢🥹😭😿"
+    }
+}
+
+// Este es el codigo del primer reto tal cual
+
+function changeBorderRed() {
+    let imageBorder;
+    imageBorder = document.querySelector("#map-img-border");
+    if (imageBorder.style.border = "0px") {
+        imageBorder.style.border = "2px"
+    }
+    else if (imageBorder.style.border != "0px") {
+        imageBorder.style.border = "0px";
+    }
+}
